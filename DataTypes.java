34f0e847e6ab60e549b4d837c1c2810842e62057
sandeep2297 <sandybhndr05@gmail.com>
2019-10-23 13:07:20 +0530

    Added datatypes program

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+class DataTypes
+{
+	public static void main(String[] args)
+	{
+	System.out.println("Size of each DataTypes:");
+
+ 	System.out.println("Size of byte: " + (Byte.SIZE/8) + " bytes.");
+ 	System.out.println("Size of short: " + (Short.SIZE/8) + " bytes.");
+ 	System.out.println("Size of int: " + (Integer.SIZE/8) + " bytes.");
+ 	System.out.println("Size of long: " + (Long.SIZE/8) + " bytes.");
+ 	System.out.println("Size of Float: " + (Float.SIZE/8) + " bytes.");
+ 	System.out.println("Size of Double: " + (Double.SIZE/8) + " bytes.");
+ 	System.out.println("Size of Character: " + (Character.SIZE/8) + " bytes.");
+
+	System.out.println("Range of each DataTypes:");
+	System.out.println("Byte Min value = " + Byte.MIN_VALUE +" Byte Max value=" + Byte.MAX_VALUE);
+	System.out.println("Short Min value = " + Short.MIN_VALUE +" Short Max value=" + Short.MAX_VALUE);
+	System.out.println("Integer Min value = " + Integer.MIN_VALUE +" Integer Max value=" + Integer.MAX_VALUE);
+	System.out.println("Long Min value = " + Long.MIN_VALUE +" Long Max value=" + Long.MAX_VALUE);
+	System.out.println("Float Min value = " + Float.MIN_VALUE +" Float Max value=" + Float.MAX_VALUE);
+	System.out.println("Double Min value = " + Double.MIN_VALUE +" Double Max value=" + Double.MAX_VALUE);
+	System.out.println("Character Min value = " + Character.MIN_VALUE +" Character Max value=" + Character.MAX_VALUE);
+
+	}
+}
